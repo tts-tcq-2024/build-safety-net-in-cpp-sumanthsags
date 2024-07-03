b6f7f28f224a555b71f73d5906fad57b506cca8a
sumanthsags <120707040+sumanthsags@users.noreply.github.com>
2024-07-03 20:04:25 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -7,6 +7,6 @@ TEST(SoundexTest, HandlesEmptyString) {
 }
 
 TEST(SoundexTest, HandlesSingleCharacter) {
-    EXPECT_EQ(generateSoundex("A"), "A000");
+    EXPECT_EQ(generateSoundex("HERMAN"), "A000");
 }
 
