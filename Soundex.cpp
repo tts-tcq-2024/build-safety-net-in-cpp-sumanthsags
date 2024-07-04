95fd77af2a8318f6df02fa174f7645688d26d488
sumanthsags <120707040+sumanthsags@users.noreply.github.com>
2024-07-04 12:36:39 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -3,7 +3,7 @@
 
 char getSoundexCode(char c)
 {
-    static const char soundexCodes[26] = {0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 3, 0, 0, 0, 0, 0, 0, 0, 2};
+    static const char soundexCodes[26] = {'0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5', '0', '1', '2', '6', '2', '3', '0', '1', '0', '2', '0', '2'};
     return soundexCodes[toupper(c) - 'A'];
 }
 
@@ -19,7 +19,7 @@ char eliminateZeroandRepeatedValue (char code, char prevcode, std::string& sound
 
 char getStringCode(const std::string& name, std::string& soundex, char prevcode)
 {
-    for (size_t i = 1; i < soundex.length() < 4; ++i)
+    for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i)
     {
         char code = getSoundexCode(name[i]);
         char prevcode = eliminateZeroandRepeatedValue(code, prevcode, soundex);    
@@ -31,7 +31,7 @@ std::string generateSoundex(const std::string& name)
 {
     if (name.empty()) return "";
 
-    std::string soundex(4, toupper(name[0]));
+    std::string soundex(1, toupper(name[0]));
     char prevCode = getSoundexCode(name[0]);
 
     prevCode = getStringCode(name, soundex, prevCode);
